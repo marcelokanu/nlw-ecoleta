6049ff41b77f8d0ce9273b8c2769341aa8fc2cf1
marcelokanu <marcelokanu@gmail.com>
2020-06-03 09:28:42 -0400

    aula 2

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,14 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('listagem de usuarios');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  res.json([
-    'Marcelo',
-    'Carol',
-    'Sophia',
-  ]);
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
