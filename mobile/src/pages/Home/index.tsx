c493e1221c401d09b488a9cf8b720a96479d6851
marcelokanu <marcelokanu@gmail.com>
2020-06-05 00:51:04 -0400

    aula 4

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,217 @@
+import React, { useEffect, useState, ChangeEvent } from "react";
+import {
+  View,
+  ImageBackground,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { Feather as Icon } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import RNPickerSelect from "react-native-picker-select";
+import axios from "axios";
+
+interface IIbgeUfResponse {
+  sigla: string;
+}
+
+interface IIbgeCityResponse {
+  nome: string;
+}
+
+const Home = () => {
+  //Usando Input
+  const [uf, setUf] = useState('');
+  const [city, setCity] = useState('');
+
+  const navigation = useNavigation();
+  
+  function handleNavigationToPoints() {
+    navigation.navigate('Points', {
+      uf,
+      city
+    });
+  };
+
+  //Usando Select
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const [selectedUf, setSelectedUf] = useState("0");
+  const [selectedCity, setselectedCity] = useState("0");
+  useEffect(() => {
+    axios
+      .get<IIbgeUfResponse[]>(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome"
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+        setUfs(ufInitials);
+      });
+  }, []);
+  useEffect(() => {
+    if (selectedUf === "0") {
+      return;
+    }
+    axios
+      .get<IIbgeCityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome);
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
+  function handleSelectUf(event: ChangeEvent<HTMLSelectElement>) {
+    const uf = event.target.value;
+    setSelectedUf(uf);
+  }
+
+  function handleSelectCity(event: ChangeEvent<HTMLSelectElement>) {
+    const city = event.target.value;
+    setselectedCity(city);
+  }
+
+  return (
+    <KeyboardAvoidingView style={{flex:1}} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ImageBackground
+        source={require("../../assets/home-background.png")}
+        style={styles.container}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require("../../assets/logo.png")} />
+          <View>
+            <Text style={styles.title}>
+              Seu marketplace de coleta de resíduos
+            </Text>
+            <Text style={styles.description}>
+              Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <TextInput 
+            style={styles.input} 
+            placeholder="Digite a UF"
+            maxLength={2}
+            autoCapitalize="characters"
+            autoCorrect={false}
+            value={uf}
+            onChangeText={setUf}
+          />
+          <TextInput 
+            style={styles.input} 
+            placeholder="Digite a Cidade"
+            autoCorrect={false}
+            value={city}
+            onChangeText={setCity}
+          />
+
+          <RNPickerSelect
+            onValueChange={(value) => setUfs}
+            items={
+              [
+                { label: "SC", value: "SC" },
+                { label: "MS", value: "MS" },
+              ]
+            }
+          />
+            <RNPickerSelect
+            onValueChange={(value) => setUfs}
+            items={[
+              { label: "Rio do sul", value: "Rio do sul" },
+              { label: "São Gabriel do Oeste", value: "São Gabriel do Oeste" },
+            ]}
+          />
+
+          <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name="arrow-right" color="#FFF" size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#322153",
+    fontSize: 32,
+    fontFamily: "Ubuntu_700Bold",
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: "#6C6C80",
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: "Roboto_400Regular",
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: "#34CB79",
+    height: 60,
+    flexDirection: "row",
+    borderRadius: 10,
+    overflow: "hidden",
+    alignItems: "center",
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#FFF",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 16,
+  },
+});
